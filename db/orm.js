a8efea059999b143a04fec8a2a0d506983d73dcf
JaceKace <jasonkitamura@gmail.com>
2020-03-11 13:52:21 -0400

    running server

---
+++ db/orm.js
@@ -0,0 +1,7 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require ( 'bcrypt' );
+
+// mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {useNewUrlParser: true, useUnifiedTopology: true,});
+
+// // include mongoose models (it will include each file in the models directory)
+// // const db = require( './models' );
