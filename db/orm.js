26a585afca9f958703c5ae5e2c5e0428f3d90688
JaceKace <jasonkitamura@gmail.com>
2020-03-15 13:22:20 -0400

    unstring connection port to db

--- db/orm.js
+++ db/orm.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require ( 'bcrypt' );
 require('dotenv').config()
 
-mongoose.connect(`process.env.MONGODB_URI || mongodb://localhost:27017/undefined`, {useNewUrlParser: true, useUnifiedTopology: true,});
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/undefined', {useNewUrlParser: true, useUnifiedTopology: true,});
 mongoose.set('useCreateIndex', true);
 
 // include mongoose models (it will include each file in the models directory)
