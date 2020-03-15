180d826baa72b18d1cd2122f71a3d002d91e5ec0
JaceKace <jasonkitamura@gmail.com>
2020-03-15 11:58:10 -0400

    pushing to heroku

--- db/orm.js
+++ db/orm.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require ( 'bcrypt' );
+require('dotenv').config()
 
-mongoose.connect(`mongodb://localhost:27017/undefined`, {useNewUrlParser: true, useUnifiedTopology: true,});
+mongoose.connect(`process.env.MONGODB_URI || mongodb://localhost:27017/undefined`, {useNewUrlParser: true, useUnifiedTopology: true,});
 mongoose.set('useCreateIndex', true);
 
 // include mongoose models (it will include each file in the models directory)
